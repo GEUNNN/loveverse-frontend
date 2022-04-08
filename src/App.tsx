fbfec5565a229f9f7cfb7b2587e29c32ad75d07d
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-04-08 23:46:14 +0900

    feat: get mock data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import axios from "axios";
 import React from "react";
 
 function App() {
+  const result = axios.get("http://localhost:3001/data/main.json");
+  result.then((res) => console.log(res));
+
   return <div className="App">loveverse 안뇽</div>;
 }
 
