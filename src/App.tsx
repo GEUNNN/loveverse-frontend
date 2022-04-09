059f547ffae583e0c9db2e4778a6bd1ecd503532
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-04-09 23:40:53 +0900

    feat: add type and store response data to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
+
+export interface ArtistResData {
+  id: number;
+  artistName: string;
+  image: string;
+}
 
 function App() {
+  const [artists, setArtists] = useState<ArtistResData[]>([]);
+
   const result = axios.get("http://localhost:3001/data/main.json");
-  result.then((res) => console.log(res));
+  result.then((res) => setArtists(res?.data.result));
+
+  console.log("artist", artists);
 
   return <div className="App">loveverse 안뇽</div>;
 }
