af0e2676d7e507582741d49b96c0baefa38d36f9
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-04-11 23:25:03 +0900

    feat: add logo and button to nav

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { ReactComponent as Logo } from "./assets/logo.svg";
 
 export interface ArtistResData {
   id: number;
@@ -15,7 +16,15 @@ function Main() {
 
   console.log("artist", artists);
 
-  return <div className="App">loveverse 안뇽</div>;
+  return (
+    <div className="App">
+      <nav>
+        <Logo />
+        <button>회원가입</button>
+        <button>로그인</button>
+      </nav>
+    </div>
+  );
 }
 
 export default Main;
