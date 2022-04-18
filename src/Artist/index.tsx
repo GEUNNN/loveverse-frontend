152b4a1575fa7e8c63394062e0f92e2fceffa350
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-04-18 23:54:27 +0900

    feat: add route and artist index component

---
+++ src/Artist/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ArtistIndex() {
+  return <div>artist page</div>;
+}
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ArtistIndex from "./Artist";
 import Main from "./Main";
 
 export default function Router(): JSX.Element {
@@ -7,6 +8,7 @@ export default function Router(): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/:artistName" element={<ArtistIndex />} />
       </Routes>
     </BrowserRouter>
   );
